713a6c44af155cca269f170cd9afc23fa15a075e
gallochri <chri@gallochri.com>
2016-09-15 20:40:22 +0200

    Tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,78 @@
 #include <Arduino.h>
+
+/*
+Many thanks to nikxha from the ESP8266 forum
+*/
+
+#include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define SS_PIN 10
-#define RST_PIN 9
+/* wiring the MFRC522 to ESP8266 (ESP-12)
+RST     = GPIO15
+SDA(SS) = GPIO2
+MOSI    = GPIO13
+MISO    = GPIO12
+SCK     = GPIO14
+GND     = GND
+3.3V    = 3.3V
+*/
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+#define RST_PIN	15 // RST-PIN für RC522 - RFID - SPI - Modul GPIO15
+#define SS_PIN	2  // SDA-PIN für RC522 - RFID - SPI - Modul GPIO2
 
-MFRC522::MIFARE_Key key;
+const char *ssid =	"yourSSID";	    // change according to your Network - cannot be longer than 32 characters!
+const char *pass =	"yourPASSWORD";	// change according to your Network
 
-// Init array that will store new NUID
-byte nuidPICC[3];
+MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance
 
-/**
- * Helper routine to dump a byte array as hex values to Serial.
- */
-void printHex(byte *buffer, byte bufferSize) {
+// Helper routine to dump a byte array as hex values to Serial
+void dump_byte_array(byte *buffer, byte bufferSize) {
     for (byte i = 0; i < bufferSize; i++) {
         Serial.print(buffer[i] < 0x10 ? " 0" : " ");
         Serial.print(buffer[i], HEX);
     }
 }
 
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-        Serial.print(buffer[i], DEC);
-    }
-}
-
 void setup() {
-    Serial.begin(9600);
-    SPI.begin(); // Init SPI bus
-    rfid.PCD_Init(); // Init MFRC522
+    Serial.begin(9600);    // Initialize serial communications
+    delay(250);
+    Serial.println(F("Booting...."));
 
-    for (byte i = 0; i < 6; i++) {
-        key.keyByte[i] = 0xFF;
+    SPI.begin();	         // Init SPI bus
+    mfrc522.PCD_Init();    // Init MFRC522
+
+    WiFi.begin(ssid, pass);
+
+    int retries = 0;
+    while ((WiFi.status() != WL_CONNECTED) && (retries < 10)) {
+        retries++;
+        delay(500);
+        Serial.print(".");
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println(F("WiFi connected"));
     }
 
-    Serial.println(F("This code scan the MIFARE Classsic NUID."));
-    Serial.print(F("Using the following key:"));
-    printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+    Serial.println(F("Ready!"));
+    Serial.println(F("======================================================"));
+    Serial.println(F("Scan for Card and print UID:"));
 }
 
 void loop() {
-
     // Look for new cards
-    if (!rfid.PICC_IsNewCardPresent())
-        return;
-
-    // Verify if the NUID has been readed
-    if (!rfid.PICC_ReadCardSerial())
-        return;
-
-    Serial.print(F("PICC type: "));
-    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-    Serial.println(rfid.PICC_GetTypeName(piccType));
-
-    // Check is the PICC of Classic MIFARE type
-    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
-        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-        piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-        Serial.println(F("Your tag is not of type MIFARE Classic."));
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        delay(50);
         return;
     }
-
-    if (rfid.uid.uidByte[0] != nuidPICC[0] ||
-        rfid.uid.uidByte[1] != nuidPICC[1] ||
-        rfid.uid.uidByte[2] != nuidPICC[2] ||
-        rfid.uid.uidByte[3] != nuidPICC[3]) {
-        Serial.println(F("A new card has been detected."));
-
-        // Store NUID into nuidPICC array
-        for (byte i = 0; i < 4; i++) {
-            nuidPICC[i] = rfid.uid.uidByte[i];
-        }
-
-        Serial.println(F("The NUID tag is:"));
-        Serial.print(F("In hex: "));
-        printHex(rfid.uid.uidByte, rfid.uid.size);
-        Serial.println();
-        Serial.print(F("In dec: "));
-        printDec(rfid.uid.uidByte, rfid.uid.size);
-        Serial.println();
-    } else Serial.println(F("Card read previously."));
-
-    // Halt PICC
-    rfid.PICC_HaltA();
-
-    // Stop encryption on PCD
-    rfid.PCD_StopCrypto1();
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+        delay(50);
+        return;
+    }
+    // Show some details of the PICC (that is: the tag/card)
+    Serial.print(F("Card UID:"));
+    dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
+    Serial.println();
 }
+
